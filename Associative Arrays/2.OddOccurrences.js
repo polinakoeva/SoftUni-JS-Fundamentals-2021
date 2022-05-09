0cdc2383fedcb8f86140b73cc00eb2e3a161eb1a
Polina Koeva <polina.koeva@gmail.com>
2022-05-09 22:56:57 +0200

    Add files via upload

---
+++ Associative Arrays/2.OddOccurrences.js
@@ -0,0 +1,27 @@
+function oddOccurrences(input){
+
+    let words = input.toLowerCase().split(' ');
+    let map = new Map();
+
+    for(let word of words){
+        let num = 0;
+        if(map.has(word)){
+            num = map.get(word)
+        }
+        num++;
+        map.set(word, num);
+    }
+    let entries = Array.from(map);
+    let filtredElements = entries.filter((el)=>{
+        return el[1] % 2 !==0;
+    });
+    let result = [];
+
+    for(let word of filtredElements){
+        result.push(word[0]);
+    }
+    console.log(result.join(' '));
+    
+
+}
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
